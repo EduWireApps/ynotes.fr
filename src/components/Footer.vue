cf74e4644643b207c484c133657cab8aa0c8c5b3
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-04-03 19:57:30 +0200

    improve footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,26 @@
   <footer
     class="mt-auto border-t border-gray-300 bg-white px-2 py-4 text-gray-500"
   >
-    <div class="max-w-4xl mx-auto">
-      &copy; {{ new Date().getFullYear() }} - Tous droits réservés
+    <div class="max-w-4xl mx-auto flex justify-between">
+      <div class="span">
+        &copy; {{ new Date().getFullYear() }} - Tous droits réservés
+      </div>
+      <div class="flex space-x-4">
+        <a
+          href="/legal/pdc.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="hover:text-gray-700"
+          >Politique de confidentialité</a
+        >
+        <a
+          href="/legal/cgu.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="hover:text-gray-700"
+          >Conditions d'utilisation</a
+        >
+      </div>
     </div>
   </footer>
 </template>
