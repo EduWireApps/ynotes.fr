11f06732f9f29b929b8913ae97cca447872436dd
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-04-03 19:43:07 +0200

    add timeline

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -44,10 +44,87 @@
         </div>
       </div>
     </div>
+    <div class="py-16 bg-primary-400">
+      <div class="max-w-4xl mx-auto">
+        <h2 class="text-5xl text-white font-bold mb-12">Chronologie</h2>
+        <div class="flex flex-col grid-cols-9 p-2 mx-auto md:grid">
+          <div
+            v-for="(e, i) in events"
+            :key="i"
+            class="flex md:contents"
+            :class="{ 'flex-row-reverse': i % 2 === 0 }"
+          >
+            <div
+              v-if="i % 2 !== 0"
+              class="relative col-start-5 col-end-6 mr-7 md:mx-auto"
+            >
+              <div class="flex items-center justify-center w-6 h-full">
+                <div
+                  class="w-1 h-full"
+                  :class="[
+                    i === 0 ? 'rounded-t-full bg-gradient-to-b' : '',
+                    i === events.length - 1
+                      ? 'rounded-b-full bg-gradient-to-t'
+                      : '',
+                    i === 0 || i === events.length - 1
+                      ? 'from-primary-400 to-primary-300'
+                      : 'bg-primary-300',
+                  ]"
+                ></div>
+              </div>
+              <div
+                class="absolute w-6 h-6 -mt-3 bg-white border-4 border-primary-400 rounded-full top-1/2"
+              ></div>
+            </div>
+            <div
+              class="relative p-4 my-6 text-gray-800 bg-white rounded-xl"
+              :class="
+                i % 2 === 0
+                  ? 'col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto'
+                  : 'col-start-6 col-end-10 mr-auto'
+              "
+            >
+              <h3
+                class="text-lg font-semibold lg:text-xl"
+                v-html="e.title"
+              ></h3>
+              <p v-if="e.content" class="mt-2 leading-6" v-html="e.content"></p>
+              <span
+                class="absolute text-sm text-primary-200 -top-5 left-2 whitespace-nowrap"
+                v-html="e.date"
+              ></span>
+            </div>
+            <div
+              v-if="i % 2 === 0"
+              class="relative col-start-5 col-end-6 mr-7 md:mx-auto"
+            >
+              <div class="flex items-center justify-center w-6 h-full">
+                <div
+                  class="w-1 h-full bg-primary-300"
+                  :class="[
+                    i === 0 ? 'rounded-t-full bg-gradient-to-b' : '',
+                    i === events.length - 1
+                      ? 'rounded-b-full bg-gradient-to-t'
+                      : '',
+                    i === 0 || i === events.length - 1
+                      ? 'from-primary-400 to-primary-300'
+                      : 'bg-primary-300 ',
+                  ]"
+                ></div>
+              </div>
+              <div
+                class="absolute w-6 h-6 -mt-3 bg-white border-4 border-primary-400 rounded-full top-1/2"
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import TheEnd from "../assets/theEnd.md";
 import contributors from "../assets/contributors.json";
+import events from "../assets/events.json";
 </script>
