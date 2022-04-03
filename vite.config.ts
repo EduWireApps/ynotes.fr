2eacbc0c8369b573ca19a8fa87f09c817d33bbf1
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-04-03 20:06:08 +0200

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import Markdown from "vite-plugin-md";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ynotes.fr/",
   plugins: [vue({ include: [/\.vue$/, /\.md$/] }), Markdown()],
 });
